f72d36d862052dbb91ad56dc38f8e497d50e9427
Rachel Crosley <rmcrosley@alaska.edu>
2018-02-12 14:47:14 -0900

    first commit

---
+++ Battleship2.0/battleship2.0.cpp
@@ -0,0 +1,33 @@
+//  Rachel Crosley
+//  battleship2.0.cpp
+//  Battleship2.0
+//  2/9/18
+
+#include "battleship2.0.h"
+
+#include <iostream>
+#include <map>
+#include <string>
+using std::map;
+using std::string;
+
+
+
+
+
+
+
+
+int main() {
+    
+    map<string, bool> compGameBoard;
+    map<string, bool> playerGameBoard;
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
---
+++ Battleship2.0/battleship2.0.h
@@ -0,0 +1,27 @@
+//  Rachel Crosley
+//  battleship2.0.cpp
+//  Battleship2.0
+//  2/9/18
+
+
+#ifndef battleship2_0_h
+#define battleship2_0_h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* battleship2_0_h */
