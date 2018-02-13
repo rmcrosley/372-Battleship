919b77510bf20f01086373b693bcca646889066d
Rachel Crosley <rmcrosley@alaska.edu>
2018-02-12 20:47:53 -0900

    working on ship class

---
+++ Battleship2.0/Battle_TESTS.cpp
@@ -0,0 +1,21 @@
+//
+
+#include <stdio.h>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+
+TEST_CASE("location") {
+   // REQUIRE(<#...#>)
+}
+
+
+
+
+
+
+
+
+
+
--- Battleship2.0/battleship2.0.cpp
+++ Battleship2.0/battleship2.0.cpp
@@ -7,18 +7,42 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include "battleship2.0.h"
-
-// Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
+#include "catch.hpp"
+#include <map>
+#include <utility>
+#include <string>
+using std::string;
+using std::map;
+using std::pair;
 
-int main()
-{
-    
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Battleship");
 
+
+class Ships {
+public:
+    Ships(int col, int row, string name) {
+        shipName = name;
+        location.first = col;
+        location.second = row;
+    }
     
     
     
+    
+    
+    
+private:
+    pair<int, int> location;
+    string shipName;
+};
+
+
+
+
+int main() {
+    
+    map<string, pair<int, int>> shipComp;
+    map<string, pair<int, int>> shipUser;
     
     
     
